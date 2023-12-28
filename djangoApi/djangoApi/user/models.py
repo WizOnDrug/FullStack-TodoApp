f95cb18840086729f6960b14829dd7964491558f
Wiz <im.not.ali@outlook.com>
2023-12-28 06:28:30 +0330

    configure models for user in backEnd

---
+++ djangoApi/djangoApi/user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+ 
+class UserModel(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "user"
+        ordering = ['-created_at']
