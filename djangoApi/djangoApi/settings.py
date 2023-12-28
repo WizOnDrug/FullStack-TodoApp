8bd893f52fe7cb018b5a5029e0bf7f2413ff2ee0
Wiz <im.not.ali@outlook.com>
2023-12-28 06:55:07 +0330

    Add corsheaders and corsheadersmiddleware for allow domin and connect front to api

--- djangoApi/djangoApi/settings.py
+++ djangoApi/djangoApi/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user'
+    'user',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,8 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
-
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+)
 ROOT_URLCONF = 'djangoApi.urls'
 
 TEMPLATES = [
